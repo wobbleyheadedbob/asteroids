d7887afcd7373943ac68bd3a5051a0e371951cb2
wobbleyheadedbob <wobbleyheadedbob@gmail.com>
2025-06-24 16:26:27 +0100

    first commit for the Asteroids course

---
+++ README.md
@@ -0,0 +1,2 @@
+# Asteroids Module (by WobbleyHeadedBob)
+- This repo is for the 'Build Asteroids using Python and Pygame' course on boot.dev
---
+++ main.py
@@ -0,0 +1,26 @@
+from constants import *
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black", rect=None, special_flags=0)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
