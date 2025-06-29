20eb1ccaf5e8ebfb8ebfdb5a0c2a4cfde2c7e67c
wobbleyheadedbob <wobbleyheadedbob@gmail.com>
2025-06-29 23:18:43 +0100

    Added actual asteroids and the asteroidfield (supplied)

---
+++ asteroid.py
@@ -0,0 +1,20 @@
+from constants import *
+from circleshape import CircleShape
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        #p osition, radius, and a width of 2
+        pygame.draw.circle(screen, "white", self.position , self.radius)
+
+    def move(self, dt):
+       self.position += self.velocity * dt
+
+    def update(self, dt):
+        self.move(dt)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
@@ -8,14 +10,18 @@ import pygame
 def main():
     pygame.init()
     print("Starting Asteroids!")
-
+ 
+    targets = pygame.sprite.Group() # All the asteroid objects that are spawned
     updatable = pygame.sprite.Group() # All the objects that can be updated
     drawable = pygame.sprite.Group() # All the objects that can be drawn
     
     # Set both groups as containers for the Player
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (updatable, drawable, targets)
 
     my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
     game_clock = pygame.time.Clock()
