7c375556f8ec923d880358552ff047f13d9d9980
wobbleyheadedbob <wobbleyheadedbob@gmail.com>
2025-06-30 22:55:30 +0100

    implemented rate of fire/cooldown

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
         
     # in the player class
     def triangle(self):
@@ -31,6 +32,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * delta_time
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -45,5 +47,8 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
